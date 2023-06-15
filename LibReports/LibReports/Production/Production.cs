31457dc2a3dc1986a91351197d09903594debfb9
SPAN_CHAITANYA <SPAN_CHAITANYA@SPAN_CHAITANYA>
2023-06-15 15:54:03 +0530

    Another Changes

---
+++ LibReports/LibReports/Production/Production.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace LibReports.Production
+{
+    public partial class Production : DevExpress.XtraReports.UI.XtraReport
+    {
+        public Production()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ LibReports/LibReports/Recipe/Recipe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace LibReports.Recipe
+{
+    public partial class Recipe : DevExpress.XtraReports.UI.XtraReport
+    {
+        public Recipe()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
